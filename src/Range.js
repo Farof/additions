b523ce8323906d7d5a55eed9d813d4b76b6be130
Farof <gfarof@gmail.com>
2011-08-02 23:14:48 +0200

    Simplier Range

--- src/Range.js
+++ src/Range.js
@@ -5,12 +5,8 @@
     max = max || (min ? 0 : 100);
     min = min || 0;
 
-    if (min > max) {
-      [min, max] = [max, min];
-    }
-
-    this.min = min;
-    this.max = max;
+    this.min = Math.min(min, max);
+    this.max = Math.max(min, max);
   };
 
   Number.Range.implements({
